c767449d1ecc531a5ea5c8230f7c5764bdcc0f28
peck liu <xiaoyang20170204@gmail.com>
2017-11-06 16:57:08 +0800

    feature:Show one enemy

--- test/test.js
+++ test/test.js
@@ -42,17 +42,30 @@ function render() {
     /* 便利我们上面定义的行和列，用 rowImages 数组，在各自的各个位置绘制正确的图片 */
     for (row = 0; row < numRows; row++) {
         for (col = 0; col < numCols; col++) {
-          // console.log(col);
+          // console.log(col * 101);
+          // console.log(row * 83);
             /* 这个 canvas 上下文的 drawImage 函数需要三个参数，第一个是需要绘制的图片
              * 第二个和第三个分别是起始点的x和y坐标。我们用我们事先写好的资源管理工具来获取
              * 我们需要的图片，这样我们可以享受缓存图片的好处，因为我们会反复的用到这些图片
              */
-             console.log(rowImages[row]);
+            //  console.log(rowImages[row]);
             ctx.drawImage(Resources.get(rowImages[row]), col * 101, row * 83);
         }
     }
 
-    // renderEntities();
+    renderEntities();
+}
+
+/* 这个函数会在每个时间间隙被 render 函数调用。他的目的是分别调用你在 enemy 和 player
+ * 对象中定义的 render 方法。
+ */
+function renderEntities() {
+    /* 遍历在 allEnemies 数组中存放的作于对象然后调用你事先定义的 render 函数 */
+    allEnemies.forEach(function(enemy) {
+        enemy.render();
+    });
+
+    player.render();
 }
 
 /* 这个函数用来让开发者拿到他们已经加载的图片的引用。如果这个图片被缓存了，
@@ -67,7 +80,6 @@ function get(url) {
 function load(urlOrArray) {
   if (urlOrArray instanceof Array ) {
     urlOrArray.forEach(function (url) {
-      console.log(url);
       _load(url);
     })
   } else {
@@ -156,7 +168,8 @@ function main() {
     /* 调用我们的 update / render 函数， 传递事件间隙给 update 函数因为这样
      * 可以使动画更加顺畅。
      */
-    // update(dt);
+    // 这里是先调整位置然后再重新 render 绘图
+    update(dt);
     render();
 
     /* 设置我们的 lastTime 变量，它会被用来决定 main 函数下次被调用的事件。 */
@@ -165,5 +178,78 @@ function main() {
     /* 在浏览准备好调用重绘下一个帧的时候，用浏览器的 requestAnimationFrame 函数
      * 来调用这个函数
      */
-    win.requestAnimationFrame(main);
+    // 这个函数不停地重新绘制页面
+    window.requestAnimationFrame(main);
 }
+
+/* 这个函数被 main 函数（我们的游戏主循环）调用，它本身调用所有的需要更新游戏角色
+ * 数据的函数，取决于你怎样实现碰撞检测（意思是如何检测两个角色占据了同一个位置，
+ * 比如你的角色死的时候），你可能需要在这里调用一个额外的函数。现在我们已经把这里
+ * 注释了，你可以在这里实现，也可以在 app.js 对应的角色类里面实现。
+ */
+function update(dt) {
+    updateEntities(dt);
+    // checkCollisions();
+}
+
+/* 这个函数会遍历在 app.js 定义的存放所有敌人实例的数组，并且调用他们的 update()
+ * 函数，然后，它会调用玩家对象的 update 方法，最后这个函数被 update 函数调用。
+ * 这些更新函数应该只聚焦于更新和对象相关的数据/属性。把重绘的工作交给 render 函数。
+ */
+function updateEntities(dt) {
+    allEnemies.forEach(function(enemy) {
+        enemy.update(dt);
+    });
+    // player.update();
+}
+
+
+// 这是我们的玩家要躲避的敌人
+var Enemy = function() {
+    // 要应用到每个敌人的实例的变量写在这里
+    // 我们已经提供了一个来帮助你实现更多
+
+    // 敌人的图片或者雪碧图，用一个我们提供的工具函数来轻松的加载文件
+    this.sprite = 'images/enemy-bug.png';
+};
+
+// 此为游戏必须的函数，用来更新敌人的位置
+// 参数: dt ，表示时间间隙
+Enemy.prototype.update = function(dt) {
+    // 你应该给每一次的移动都乘以 dt 参数，以此来保证游戏在所有的电脑上
+    // console.log(this);
+    // 都是以同样的速度运行的
+    this.render();
+};
+
+// 此为游戏必须的函数，用来在屏幕上画出敌人，
+Enemy.prototype.render = function() {
+  // console.log('1111');
+  // console.log(Resources.get(this.sprite));
+  // console.log(this.x);
+    ctx.drawImage(Resources.get(this.sprite), 101, 83);
+};
+
+// 现在实现你自己的玩家类
+// 这个类需要一个 update() 函数， render() 函数和一个 handleInput()函数
+let e1 = new Enemy();
+// console.log(e1);
+allEnemies = [e1];
+
+// 现在实例化你的所有对象
+// 把所有敌人的对象都放进一个叫 allEnemies 的数组里面
+// 把玩家对象放进一个叫 player 的变量里面
+
+
+// 这段代码监听游戏玩家的键盘点击事件并且代表将按键的关键数字送到 Play.handleInput()
+// 方法里面。你不需要再更改这段代码了。
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
